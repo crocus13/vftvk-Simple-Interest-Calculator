74aacbbe27169fb0503bce9e1778feae58e66c9d
crocus13 <veenanidish@gmail.com>
2022-01-06 11:40:43 -0800

    Final commit

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
-function compute()
+function showMessage()
 {
-    p = document.getElementById("principal").value;
+	document.getElementById("result").style.display = "block";
+};
+
+function compute() 
+{
+	var principal = document.getElementById("principal").value;
+	if(parseInt(principal) < 1) //Validation to enter only positive values as the principal amount
+    {  
+		alert("Enter a positive number");
+		document.getElementById("principal").focus();
+		return;
+	}
+
+
+	var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var intamount = principal * years * rate / 100;//Calculate the interest amount
+    var year = new Date().getFullYear()+parseInt(years);//No of years coverted into actual year
     
+
+    document.getElementById("result").innerHTML = "If you deposit " + "<span class='highlight'>" + principal +"</span>,"+ 
+    "<br> at an interest rate of "+ "<span class='highlight'>" + rate + "</span>%." + 
+    "<br> You will receive an amount of " + "<span class='highlight'>" + intamount +"</span>," +
+    "<br> in the year " + "<span class='highlight'>" + year + "</span>.";
+
+    
+    showMessage();
+    return false;
+};
+
+
+//Select and display the value of the range slider
+
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value; 
+    rateval = rateval + "%";
+    document.getElementById("rate_val").innerText = rateval;
 }
-        
